0c6b97a10f07b74479d60b5ffacabff699de68ed
FiliTol <filippotolin@protonmail.com>
2025-08-14 19:13:51 +0200

    Update: optimized bitcounter algorithm and inserted comment to explain the bit flipping optimization process using the AND mask

--- chapter02/other/bitcounting.c
+++ chapter02/other/bitcounting.c
@@ -22,3 +22,28 @@ int bitCount(unsigned x) {
     return b;
 }
 
+// x &= (x-1) --> x = x & (x-1)
+// Faster Bitcount version
+// This version is based on the principle that ANDing a number with its previous value
+// will always remove the rightmost 1-bit from the AND operation result.
+// This happens because given an X value (ex 1100) its previous value x-1 is,
+// following the example, 1011. You get the -1 value by flipping to zero the rightmost
+// 1-bit and turning all the following bits to 1.
+// Given this transformation, when you AND this value with the original, all the
+// bits from that flipped 1 bit - and on to the right - will not satisfy the AND
+// condition, thus transforming the binary result into a number with the rightmost
+// bit removed.
+// This operation is carried on as long as the number does not fall to zero, that
+// is when the last rightmost 1 bit is the last 1 bit left to flip.
+// When that is finally flipped, the while condition gets unmatched and the loop stops,
+// returning the accumulator up to that point.
+
+int fasterBitCount(unsigned x) {
+    int counter = 0;
+    while (x != 0){
+        x &= (x-1);
+        ++counter;
+    }
+    return counter;
+}
+
