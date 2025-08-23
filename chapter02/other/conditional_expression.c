1540b8bea9e3d28b3cbff40e5260e0edd711d738
FiliTol <filippotolin@protonmail.com>
2025-08-23 17:59:41 +0200

    Update

--- chapter02/other/conditional_expression.c
+++ chapter02/other/conditional_expression.c
@@ -18,11 +18,35 @@
 //}
 
 //// Conditional expressions represent a solution to the const dynamic definition
+/*
 int main(void) {
     int a = 10;
     int b = 17;
     const int z = (a > b) ? a : b;
     printf("%d\n", z);
     return 0;
+}
+*/
+
+char lower(char a);
+
+int main(void) {
+    printf("%c\n", lower('B'));
+    return 0;
+}
+
+/* OLD VERSION: convert c to lower case; ASCII only
+int lower(int c)
+{
+if (c >= 'A' && c <= 'Z') {
+    return c + 'a' - 'A';
+} else {
+    return c
+};
+}
+*/
+
+char lower(char a) {
+    return (a >= 'A' && a <= 'Z') ? (a + 'a' - 'A') : a;
 }
 
