a6ae2dc04166502a4d0af51af1899f9b0e79c717
FiliTol <filippotolin@protonmail.com>
2025-08-27 07:11:17 +0200

    Update

---
+++ chapter03-controlFlow/sorting_algorithms/insertion_sorting.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void insertionSort(int array[], int array_size) {
+
+int main(void) {
+    return 0;
+}
+
+
+void insertionSort(int array[], int array_size) {
+    int index;
+    for (index = 1; index < array_size ; ++i) {
+        while (array[index] < array[index - 1]){ 
+
+        }
+    }
+}
+
+
