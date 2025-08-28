db9441fc64a4a7d12d88ef9009ff203012a95bc4
FiliTol <filippotolin@protonmail.com>
2025-08-28 08:41:24 +0200

    Shell sorting implementation

--- chapter03-controlFlow/sorting_algorithms/insertion_sorting.c
+++ chapter03-controlFlow/sorting_algorithms/insertion_sorting.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+
+clock_t start, end;
+double cpu_time_used;
 
 void insertionSort(int array[], int array_size);
 
 int main(void) {
-    int array[] = {1, 2, 5, 12, 11, 10, 4, 3};
+    int array[] = {1, 2, 5, 12, 11, 10, 4, 3, 100, 99, 36, 65, 83, 789, 22, 34, 55, 31, 13, 21, 91};
     int array_size = sizeof(array)/sizeof(array[0]);
 
     int i;
@@ -11,15 +15,17 @@ int main(void) {
     for (i = 0; i < array_size; ++i) {
         printf(" %d", array[i]);
     }
-
+    start = clock();
     insertionSort(array, array_size);
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
     printf("\nSorted array is: ");
     for (i = 0; i < array_size; ++i) {
         printf(" %d", array[i]);
     }
 
-    printf("\n");
+    printf("\nTook %f seconds to execute", cpu_time_used);
     return 0;
 }
 
