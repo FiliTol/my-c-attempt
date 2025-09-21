a7a3a0c3733cd4a2d4b46f28e799596f615cdab7
FiliTol <filippotolin@protonmail.com>
2025-09-21 10:50:22 +0200

    Update

--- chapter04-FunctionsAndProgramStructure/calculator/getop.c
+++ chapter04-FunctionsAndProgramStructure/calculator/getop.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
 #include <ctype.h>
+
+#define NUMBER '0'
+#define BUFFERSIZE 100
 
 int getch(void);
 void ungetch(int);
@@ -9,6 +13,32 @@ void ungetch(int);
 // For this purpose, spaces and tabs shall be skipped
 int getop(char s[])
 {
-    return '0';
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t');
+    if (!isdigit(c) && c != '.') {
+        s[1] = '\0';
+        return c;
+    }
+    i = 0;
+    if (isdigit(c)) {
+        while (isdigit(s[++i] = c = getch()));
+    }
+    if (c == '.') {
+        while (isdigit(s[++i] = c = getch()));
+    }
+
+    s[i] = '\0';
+    if ( c != EOF ) {
+        ungetch(c);
+    }
+    return NUMBER;
 }
 
+int getch(void) {
+
+}
+
+
+
+void ungetch(int);
